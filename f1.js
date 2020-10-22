782615fae43e46d2859fb12feb083e68f21b5051
Doğan Aker <61690211+doganaker@users.noreply.github.com>
2020-10-22 20:24:01 +0300

    seasons

---
+++ f1.js
@@ -0,0 +1,14 @@
+const url = "https://ergast.com/api/f1/seasons.json";
+
+export const f1seasonmanager = {
+
+    getseasons: async (limit) => {
+
+        let result = await fetch(url + '?limit=' + limit)
+        .then((res) => res.json())
+        .then((data) => data.MRData)
+
+        return result;
+    }
+
+}
