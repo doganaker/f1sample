22188789e0b858f91f5454f2238a3039bc97be7a
Doğan Aker <61690211+doganaker@users.noreply.github.com>
2020-10-22 20:25:13 +0300

    constructors

--- f1.js
+++ f1.js
@@ -1,14 +1,12 @@
-const url = "https://ergast.com/api/f1/seasons.json";
+const url = "https://ergast.com/api/f1/constructors.json?limit=";
 
-export const f1seasonmanager = {
+export const consmanager = {
 
-    getseasons: async (limit) => {
-
-        let result = await fetch(url + '?limit=' + limit)
-        .then((res) => res.json())
-        .then((data) => data.MRData)
+    getraces: async (limit) => {
+        let result = await fetch(url + limit)
+            .then((res) => res.json())
+            .then((data) => data.MRData)
 
         return result;
     }
-
 }
