b45fbbdbdb5907f1a949caedb54785ab43b2dd26
Doğan Aker <m.doganaker@gmail.com>
2020-11-19 15:31:48 +0300

    2020 Drivers MVC

--- F1SampleApp/F1SampleApp/Controllers/F1Drivers2020Controller.cs
+++ F1SampleApp/F1SampleApp/Controllers/F1Drivers2020Controller.cs
@@ -32,5 +32,85 @@ namespace F1SampleApp.Controllers
         {
             return View();
         }
+
+        public IActionResult CharlesLeclerc()
+        {
+            return View();
+        }
+
+        public IActionResult SergioPerez()
+        {
+            return View();
+        }
+
+        public IActionResult LandoNorris()
+        {
+            return View();
+        }
+
+        public IActionResult AlexanderAlbon()
+        {
+            return View();
+        }
+
+        public IActionResult PierreGasly()
+        {
+            return View();
+        }
+
+        public IActionResult CarlosSainz()
+        {
+            return View();
+        }
+
+        public IActionResult LanceStroll()
+        {
+            return View();
+        }
+
+        public IActionResult EstebanOcon()
+        {
+            return View();
+        }
+
+        public IActionResult SebastianVettel()
+        {
+            return View();
+        }
+
+        public IActionResult DaniilKvyat()
+        {
+            return View();
+        }
+
+        public IActionResult AntonioGiovinazzi()
+        {
+            return View();
+        }
+
+        public IActionResult KimiRaikkonen()
+        {
+            return View();
+        }
+
+        public IActionResult RomainGrosjean()
+        {
+            return View();
+        }
+
+        public IActionResult KevinMagnussen()
+        {
+            return View();
+        }
+
+        public IActionResult NicholasLatifi()
+        {
+            return View();
+        }
+
+        public IActionResult GeorgeRussell()
+        {
+            return View();
+        }
     }
 }
